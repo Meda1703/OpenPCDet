8e8cd949705d091374a899da92f88261ea810d5c
Meda1703 <lazar.meda@gmail.com>
2023-01-06 12:53:42 +0200

    Add swin transformer in backbone 2D.

--- pcdet/models/backbones_2d/__init__.py
+++ pcdet/models/backbones_2d/__init__.py
@@ -1,6 +1,7 @@
 from .base_bev_backbone import BaseBEVBackbone, BaseBEVBackboneV1
-
+from .swin_bev_backbone import SwinBEVBackbone
 __all__ = {
     'BaseBEVBackbone': BaseBEVBackbone,
-    'BaseBEVBackboneV1': BaseBEVBackboneV1
+    'BaseBEVBackboneV1': BaseBEVBackboneV1,
+    'SwinBEVBackbone': SwinBEVBackbone
 }
